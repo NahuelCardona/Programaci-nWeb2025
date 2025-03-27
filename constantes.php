5643f9980a3fef75202ece5c51d8730b9a949c29
NaheulCardona <nahuelcardona1@gmail.com>
2025-03-27 15:34:18 -0300

    constantes y sus errores

---
+++ constantes.php
@@ -0,0 +1,13 @@
+<?php
+const pi=3.1416;
+const pi=3.14;
+
+/* Warning: Constant pi already defined in C:\xampp\htdocs\Programaci-nWeb2025\constantes.php
+es el error que me marcó
+
+el error idndica que intentamos cambiar el valor de una constante que ta está definida y esto en php no se puede cambiar
+
+las constantes seria utiles si yo no necesito cambiar el valor
+*/
+
+?>
