9e00b1d288126d4771a9b0d3f8e1e9e31fecfa30
NaheulCardona <nahuelcardona1@gmail.com>
2025-03-27 14:41:04 -0300

    Actividad candidatos a variables

--- hola.php
+++ hola.php
@@ -1,4 +1,25 @@
 <?php
         echo "hola mundo 1";
-    ?>
+          /*esto es un comentario  
+            multilinea*/
+            // esto es un co mentario
+      
+                $nombres="manolo"; //textos puede ser con comillas dobles o simples
+        
+            $edad = 31; //enteros (Integer)
+
+            $altura = 1.72; // decimales usando el punto en lugar de la coma 
+            
+            $mayorEdad =true; //verdad o falso (boolean)
+
+            //$nombreCompleto="Juan Pérez" saldria error por no colocar ; al declararlo 
+            $Edad=25;
+            $estAtura=1.75;
+            $numero="10"; // está entre comillas cuando es un entero
+           // apellido=¨´Fernandez´;  no se declaró la variable con $
+            //pais_de_origen="Uruguay"; no se declaró la variable con $
+            $salario=$2000; el signo "$" no se permite delante del número declarado
+  ?>
+  
+    
     
