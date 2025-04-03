dc682a9edf49a575117b9f5a1cf554b5af31d6a6
NaheulCardona <nahuelcardona1@gmail.com>
2025-04-03 13:58:55 -0300

    diferencias entre comillas simples y dobles

---
+++ concatenacion.php
@@ -0,0 +1,12 @@
+<?php
+$emisora='Radio Disnen';
+echo "Me gusta escuchar $emisora";
+
+$emisora ='Radio Disney';
+echo 'me gusta escuchar $emisora';
+
+?>
+/* Usa comillas simples (') si no necesitas interpolar variables ni caracteres especiales.
+
+Usa comillas dobles (") si necesitas que se interpreten variables o caracteres especiales.
+*/
