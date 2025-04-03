344754ac82c0862f281a0fc742ea69b29323aa4e
NaheulCardona <nahuelcardona1@gmail.com>
2025-04-03 14:14:28 -0300

    operadores y concatenación

---
+++ operadores.php
@@ -0,0 +1,14 @@
+<?php
+$edad =1;
+$edad +=1;
+echo $edad;
+
+/* al escribir este codigo lo que hace es sumarle 1  a lo que
+este guardado en la variable */
+
+$edad =1;
+$edad -=10;
+echo $edad;~
+
+
+?>
