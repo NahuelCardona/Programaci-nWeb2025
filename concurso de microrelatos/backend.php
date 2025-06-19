e062c06931ba8f9fa37d3ad5b9bdb67580604ef1
NahuelCardona <nahuelcardona1@gmail.com>
2025-06-19 18:08:00 -0300

    Practico terminado

--- concurso de microrelatos/backend.php
+++ concurso de microrelatos/backend.php
@@ -1,15 +1,18 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $cantidadPalabra= $_POST['texto'];
-    $arregloPalabras= explode(" ",$cantidadPalabra);
-    print_r($arregloPalabras);
-
-    count($cantidadPalabra);
-    print_r($cantidadPalabra);
-    if($cantidadPalabra > 700){
-        
+    $cantidadCaracteres= $_POST['texto'];
+    $arregloPalabras= explode(" ",$cantidadCaracteres);
+    $cantidadDePalabras= count($arregloPalabras);
+    
+   
+    if($cantidadDePalabras >100 ){
+        echo"$cantidadCaracteres <br><br> El relato no cumple con uno de los requisitos debido a que <br> haz superado la cantidad maxima de palabras, vuelve a escribir otro texto. ";
+    }else{
+        echo " <br> El texto será evaluado por el jurado";
     }
 
+    
+   
 }
 ?>
